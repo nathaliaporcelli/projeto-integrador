69e0d234264b961f8d63d88d44449c7c96bfd715
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-24 21:25:37 -0300

    flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python3 manage.py migrate
+python3 manage.py collectstatic
 
-#DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
-python manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+#python manage.py createsuperuser --noinput
 gunicorn django_pi_v2.wsgi:application --bind 0.0.0.0:8000
