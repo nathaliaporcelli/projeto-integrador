a6e5ec92920dc6db10fcbd60cf332a61c9351018
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-26 21:36:57 -0300

    ajuste create super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 
 python3 manage.py migrate
 python3 manage.py collectstatic
+./manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
 
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+#DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 #python manage.py createsuperuser --noinput
+#./manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+
 gunicorn django_pi_v2.wsgi:application --bind 0.0.0.0:8000
