94b4180a0de9e37489cd13f79c23eebd4fff8ae8
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-26 22:25:17 -0300

    settins.py secrets

--- django_pi_v2/django_pi_v2/settings/stage.py
+++ django_pi_v2/django_pi_v2/settings/stage.py
@@ -20,9 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_r=7v(%21be30#9-luv^#!n_8!o^)2t30+(f(u-)z65x@e7y84'
-
-DEBUG = os.getenv('DEBUG')
+SECRET_KEY =os.getenv('SECRET_KEY')
+DEBUG =os.getenv('DEBUG')
+DJANGO_SUPER_USER_PASSWORD =os.getenv('DJANGO_SUPER_USER_PASSWORD')
+DJANGO_SUPER_USER_NAME =os.getenv('DJANGO_SUPER_USER_NAME')
+DJANGO_SUPER_USER_EMAIL=os.getenv('DJANGO_SUPER_USER_EMAIL')
 
 ALLOWED_HOSTS = ['*']
 
