9579d2eec7da6f4818ae1a4e8ddcad4bef3aa7f1
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-26 21:43:02 -0300

    ajuste manage.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 python3 manage.py migrate
 python3 manage.py collectstatic
-./manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+./django_pi_v2 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
 
 #DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 #python manage.py createsuperuser --noinput
