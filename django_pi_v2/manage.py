ada4031d1ca13733d595b0978d4755d9195fc950
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-27 22:59:15 -0300

    packege settings

--- django_pi_v2/manage.py
+++ django_pi_v2/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pi_v2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pi_v2.setings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
