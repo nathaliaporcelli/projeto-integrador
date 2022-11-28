493f17a04c140e10e89b2dab49de9614a9b4b225
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-28 08:05:37 -0300

    RM preload

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ python3 manage.py migrate  --noinput
 python3 manage.py collectstatic --noinput
 DJANGO_SUPERUSER_PASSWORD=mudar123 python3 manage.py createsuperuser --username=django_user --email=django@email.com --noinput
 
-gunicorn django_pi_v2.wsgi:application --preload --bind 0.0.0.0:8000 
+gunicorn django_pi_v2.wsgi:application --bind 0.0.0.0:8000 
 #gunicorn django_pi_v2.wsgi:application --env DJANGO_SETTINGS_MODULE='django_pi_v2.settings'--bind 0.0.0.0:8000 --password=$DJANGO_SUPER_USER_PASSWORD 
