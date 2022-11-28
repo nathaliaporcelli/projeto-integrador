a4beb55c3b985886a183fd55f65104a7ef4b8e5e
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2022-11-28 06:46:47 -0300

    wsgi

--- django_pi_v2/django_pi_v2/wsgi.py
+++ django_pi_v2/django_pi_v2/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_pi_v2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_pi_v2.settings.settings")
 
 application = get_wsgi_application()
